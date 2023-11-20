da59ef96d0c1b9abedf95755375d251e844e4509
Vlahakis Shannon (vlah) <vlah@zhaw.ch>
2023-11-20 22:07:29 +0100

    tryin

--- pages/3_DataFrame_Demo.py
+++ pages/3_DataFrame_Demo.py
@@ -1,11 +1,8 @@
 import os
 import streamlit as st
 import pandas as pd
-import json
+from google.oauth2 import service_account
 from streamlit_gsheets import GSheetsConnection
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
@@ -15,26 +12,15 @@ SAMPLE_SPREADSHEET_ID = '1ldETnrsYXn0RdrrhwdH-xW3PO_SdHQBz_v6qMf_CT9U'
 SAMPLE_RANGE_NAME = 'form_responses!A2:U'
 
 # Load credentials from credentials.json
-with open('credentials.json', 'r') as f:
-    credentials = json.load(f)
+credentials = service_account.Credentials.from_service_account_file('credentials.json', scopes=SCOPES)
 
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 # Fetch data from Google Sheets
 def fetch_google_sheets_data():
-    creds = Credentials.from_authorized_user_info(credentials, SCOPES)
-    if not creds.valid:
-        if creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
     try:
-        service = build('sheets', 'v4', credentials=creds)
+        service = build('sheets', 'v4', credentials=credentials)
         sheet = service.spreadsheets()
         result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                                     range=SAMPLE_RANGE_NAME).execute()

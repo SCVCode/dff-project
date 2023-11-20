e66b923eb9f8f4570d47e2660c79a883661b0f5e
Vlahakis Shannon (vlah) <vlah@zhaw.ch>
2023-11-20 21:36:57 +0100

    fixed something

--- pages/3_DataFrame_Demo.py
+++ pages/3_DataFrame_Demo.py
@@ -11,7 +11,6 @@ from streamlit_gsheets import GSheetsConnection
 conn = st.connection("gsheets", type=GSheetsConnection)
 
 df = conn.read()
-
 # Print results.
 for row in df.itertuples():
     st.write(f"{row.name} has a :{row.pet}:")

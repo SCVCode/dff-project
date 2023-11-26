908b3dc643d79be1e8760d324e0ec01ead87ec14
Vlahakis Shannon (vlah) <vlah@zhaw.ch>
2023-11-26 19:43:49 +0100

    fixed colums

--- pages/Overview.py
+++ pages/Overview.py
@@ -4,11 +4,7 @@ st.title("Welcome Back!")
 
 col1, col2, col3 = st.columns(3)
 
-tab1 = st.link_button("Go to Active Users", "https://streamlit.io/active_users")
-tab2 = st.link_button("Go to Non-Compliant Users", "https://streamlit.io/non_compliant_users")
-tab3 = st.link_button("Go to Clinical Alerts", "https://streamlit.io/clinicalalerts")
-
-col1.header = ("tab1")
-col2.header = ("tab2")
-col3.header = ("tab3")
+col1.link_button("Go to Active Users", "https://streamlit.io/active_users", value =120)
+col2.link_button("Go to Non-Compliant Users", "https://streamlit.io/non_compliant_users", value =120)
+col3.link_button("Go to Clinical Alerts", "https://streamlit.io/clinicalalerts", value = 120)
 
